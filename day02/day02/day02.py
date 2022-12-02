5c1229f847728e8feb10ec5ee1595ec038ea347e
Jon Fincher <jfincher42@gmail.com>
2022-12-02 10:54:42 -0600

    Day02 complete, elegant answer IMO

---
+++ day02/day02/day02.py
@@ -0,0 +1,46 @@
+# AOC 2022 Day 02
+
+import pathlib
+
+root_path = pathlib.Path.home() / "git" / "AOC2022" / "day02" / "day02"
+scores = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
+}
+
+scores2 = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
+}
+
+
+def part1(lines):
+    return sum([scores[line] for line in lines])
+
+
+def part2(lines):
+    return sum([scores2[line] for line in lines])
+
+
+if __name__ == "__main__":
+
+    with open(root_path / "input", "r") as f:
+    # with open(root_path / "sample", "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    print(f"Part 1: Answer: {part1(lines)}")
+    print(f"Part 2: Answer: {part2(lines)}")
