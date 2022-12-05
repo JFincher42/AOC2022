1a83bef499e9bd0ff5c38de5a1014b6fed9a96f7
Jon Fincher <jfincher42@gmail.com>
2022-12-05 08:47:19 -0600

    Day 05 Part 1 complete

---
+++ day05/day05/day05.py
@@ -0,0 +1,52 @@
+# AOC 2022 Day 05
+
+import pathlib
+
+root_path = pathlib.Path.home() / "git" / "AOC2022" / "day05" / "day05"
+
+def parse_move(line):
+    spl = line.split(" ")
+    return int(spl[1]), int(spl[3])-1, int(spl[5])-1
+
+def part1(lines, stacks, move):
+    while move < len(lines):
+        (count, fr, to) = parse_move(lines[move])
+        for i in range(count):
+            stacks[to].append(stacks[fr].pop())
+        move += 1
+
+    retval = ""
+    for i in range(len(stacks)):
+        retval += stacks[i].pop()
+
+    return retval
+
+
+def part2(lines, stacks, move):
+    pass
+
+def parse(lines, bottom):
+    stacks = [[] for x in range(bottom)]
+    for line in range(bottom-1, -1, -1):
+        crate = 0
+        while crate*4+1 < len(lines[line]):
+            if lines[line][crate*4+1].isupper():
+                stacks[crate].append(lines[line][crate*4+1])
+            crate += 1
+    return stacks
+
+
+if __name__ == "__main__":
+
+    with open(root_path / "input", "r") as f:
+    # with open(root_path / "sample", "r") as f:
+        lines = [line for line in f.readlines()]
+
+    # stacks = parse(lines, 3)  # For the sample input
+    stacks = parse(lines, 9)  # For the real input
+
+    # print(f"Part 1: Answer: {part1(lines, stacks, 5)}")
+    # print(f"Part 2: Answer: {part2(lines, stacks, 5)}")
+
+    print(f"Part 1: Answer: {part1(lines, stacks, 10)}")
+    # print(f"Part 2: Answer: {part2(lines, stacks, 10)}")
