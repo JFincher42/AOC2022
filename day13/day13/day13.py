87ee63c7350fe360d46bb86a22018d9a95e705ac
Jon Fincher <jfincher42@gmail.com>
2022-12-13 20:04:22 -0600

    Day 13 complete, input file added

--- day13/day13/day13.py
+++ day13/day13/day13.py
@@ -23,7 +23,7 @@ def compare_packets(left, right):
         right_value = right[i]
 
         # Figure out how to compare them
-        if type(left_value) == type(1) and type(right_value) == type(1):
+        if isinstance(left_value, int) and isinstance(right_value, int):
             # Both integers, check if left < right
             if left_value < right_value:
                 return 1
@@ -33,14 +33,18 @@ def compare_packets(left, right):
                 # They're the same, so continue
                 continue
 
-        # At this point, one of them is a list, so convert the other one
-        if type(left_value) == type(1):
+        # At this point, at least one of them is a list, so convert the other one
+        if isinstance(left_value, int):
             left_value = [left_value]
-        else:
+        elif isinstance(right_value, int):
             right_value = [right_value]
 
         # They're both lists, so we do this again
-        return compare_packets(left_value, right_value)
+        subpackets=compare_packets(left_value, right_value)
+        if subpackets==0:
+            continue
+        else:
+            return subpackets
 
     # If we're here, they were the same, so return 0
     return 0
@@ -63,13 +67,33 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    current_line = 0
+    correct_order_2 = 1
+    correct_order_6 = 2
+
+    while current_line < len(lines):
+        if lines[current_line]=="":
+            current_line += 1
+            continue
+
+        left = eval(lines[current_line])
+        right = [2]
+        if compare_packets(left, right) == 1:
+            correct_order_2 += 1
+
+        right = [6]
+        if compare_packets(left, right) == 1:
+            correct_order_6 += 1
+
+        current_line += 1
+
+    return correct_order_2 * correct_order_6
 
 
 if __name__ == "__main__":
 
-    # with open(root_path / "input", "r") as f:
-    with open(root_path / "sample", "r") as f:
+    with open(root_path / "input", "r") as f:
+    # with open(root_path / "sample", "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
     print(f"Part 1: Answer: {part1(lines)}")
