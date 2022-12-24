005c4e9cb8ce14066d9e1b14cd6b2d124f0a3d11
Jon Fincher <jfincher42@gmail.com>
2022-12-24 17:21:31 -0600

    Day24 part 1 done with help from Reddit

--- day24/day24/day24.py
+++ day24/day24/day24.py
@@ -1,100 +1,54 @@
 # AOC 2022 Day 24
 
 import pathlib
+from collections import deque
 
 
 root_path = pathlib.Path.home() / "git" / "AOC2022" / "day24" / "day24"
 
 
-# Using this as a dataclass to hold the state in one thing
-class State:
-    def __init__(self, position, moves, blizzards):
-        self.pos = position
-        self.moves = moves
-        self.blizzards = blizzards
-
-
 def parse(lines):
-    walls = set()
     blizzards = {"<": set(), ">": set(), "v": set(), "^": set()}
 
-    # Figure out where all the walls and blizzards are
+    # Figure out where all the blizzards are
     for y in range(len(lines)):
         for x in range(len(lines[y])):
-            if lines[y][x] == "#":
-                walls.add((x, y))
-            elif lines[y][x] == ".":
-                continue
-            else:
-                blizzards[lines[y][x]].add((x, y))
+            if lines[y][x] in "<>^v":
+                # We subtract 1 to get to a zero based indexing
+                blizzards[lines[y][x]].add((x - 1, y - 1))
 
-    return walls, blizzards
+    return blizzards
 
 
-def move_blizzards(blizzards, walls, maxx, maxy):
-    new_blizzards = {"<": set(), ">": set(), "v": set(), "^": set()}
-    bdirs = {"<": (-1, 0), ">": (1, 0), "v": (0, 1), "^": (0, -1)}
+def in_blizzard(x, y, mx, my, t, blizzards):
 
-    # Look at each one
-    for d in blizzards.keys():
-        for bpos in blizzards[d]:
-            newx = bpos[0] + bdirs[d][0]
-            newy = bpos[1] + bdirs[d][1]
+    # We can use some math here 
+    # Each blizzard moves in a straight line, one move per second
+    # Which means after t seconds, it's moved t spaces
+    # Which is the same as us moving t spaces the other way
+    #
+    # So if we check our position after moving t spaces in some direction
+    # We can see if a blizzard moving the opposite way is there
+    # If so, we can't be there.
+    # 
+    # This was taken from a Reddit post, but entered by hand and understood
 
-            # Did we move into a wall?
-            if (newx, newy) in walls:
-                if newx > maxx:
-                    newx = 1
-                if newx == 0:
-                    newx = maxx
-                if newy == 0:
-                    newy = maxy - 1
-                if newy >= maxy:
-                    newy = 1
-
-            new_blizzards[d].add((newx, newy))
-
-    return new_blizzards
-
-
-def draw(walls, blizzards, pos, finish):
-    for y in range(finish[1] + 1):
-        for x in range(finish[0] + 2):
-            ch = "."
-            b = 0
-
-            if (x, y) in blizzards["<"]:
-                b += 1
-                ch = "<"
-            if (x, y) in blizzards[">"]:
-                b += 1
-                ch = ">"
-            if (x, y) in blizzards["v"]:
-                b += 1
-                ch = "v"
-            if (x, y) in blizzards["^"]:
-                b += 1
-                ch = "^"
-
-            if (x, y) in walls:
-                ch = "#"
-            if (x, y) == pos:
-                ch = "E"
-
-            if b > 1:
-                print(b, end="")
-            else:
-                print(ch, end="")
-        print()
-
-    print()
+    return any(
+        (
+            (x, (y - t) % my) in blizzards["v"],
+            (x, (y + t) % my) in blizzards["^"],
+            ((x - t) % mx, y) in blizzards[">"],
+            ((x + t) % mx, y) in blizzards["<"],
+        )
+    )
 
 
 def part1(lines, start, finish):
     # Here's the thinking -- we do a BFS through the valley
     # Using BFS was a hint from Reddit, but the rest of this is mine
     #
-    # First state is our start position, zero moves, original blizzards
+    # First state is our start position and the minutes
+    # We can save this as a tuple
     #
     # We check for two things:
     # - Are we in teh same place as a blizzard?
@@ -102,7 +56,7 @@ def part1(lines, start, finish):
     # - Are we at the end?
     #   - If so, we return the number of moves we made
     #
-    # If neither are true, we check for the following moves:
+    # If neither are true, we check if the following moves are valid:
     # - up,
     # - down,
     # - left,
@@ -112,113 +66,64 @@ def part1(lines, start, finish):
     # Each state we push consists of:
     # - The position we move to
     # - How many moves this will be
-    # - The new positions of the blizzards
     #
     # Otherwise, standard BFS
 
-    walls, blizzards = parse(lines)
+    # Get the blizzard positions
+    blizzards = parse(lines)
 
-    next_loc = [State(start, 0, blizzards)]
+    # Setup the queue
+    next_loc = deque()
 
-    draw(walls, blizzards, start, finish)
+    # Keep trying to add a start position -- we may have to wait to enter
+    time = 1
+    while not next_loc:
+        if not in_blizzard(start[0], start[1], finish[0], finish[1], time, blizzards):
+            next_loc.append((start[0], start[1], time))
+        else:
+            time += 1
 
     visited = set()
 
     while len(next_loc) > 0:
-        current_loc = next_loc.pop(0)
-
-        # Get the blizzards
-        blizzards = current_loc.blizzards
-
-        # Get the current minute
-        moves = current_loc.moves
+        currx, curry, time = next_loc.popleft()
 
         # Have we been here at this time before?
-        if (current_loc.pos[0], current_loc.pos[1], current_loc.moves) in visited:
+        if (currx, curry, time) in visited:
             continue
         else:
-            visited.add((current_loc.pos[0], current_loc.pos[1], current_loc.moves))
+            visited.add((currx, curry, time))
 
         # Are we in the same place as a blizzard?
-        for _, blocs in blizzards.items():
-            if current_loc.pos in blocs:
-                # This is an invalid position, we can't be here
-                continue
-
-        # Are we in a wall (shouldn't happen)
-        if current_loc.pos in walls:
+        if in_blizzard(currx, curry, finish[0], finish[1], time, blizzards):
+            # This won't work
             continue
 
+        # Are we in a wall (shouldn't happen)
+        # if (currx, curry) in walls:
+        #     continue
+
         # Are we at the end?
-        if current_loc.pos == finish:
-            draw(walls, blizzards, current_loc.pos, finish)
+        if (currx, curry) == (finish[0]-1, finish[1]-1):
+            # draw(walls, blizzards, (currx, curry), finish)
 
             # How many moves to get here
-            return moves+1
-
-        # Otherwise, we need to check all five moves
-        # First, let's move all the blizzards
-        blizzards = move_blizzards(blizzards, walls, finish[0], finish[1])
-
-        # Let's draw everything
-        # draw(walls, blizzards, current_loc.pos, finish)
+            return time + 1
 
         # Now let's add our five new moves
         # Move right
-        newx, newy = current_loc.pos[0] + 1, current_loc.pos[1]
-        if newx <= finish[0] and (newx, newy) not in walls:
-            next_loc.append(
-                State(
-                    (newx, newy),
-                    moves + 1,
-                    blizzards,
+        moves = [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)]
+
+        for dx, dy in moves:
+            newx, newy = currx + dx, curry + dy
+            if (
+                0 <= newx < finish[0]
+                and 0 <= newy < finish[1]
+                and not in_blizzard(
+                    newx, newy, finish[0], finish[1], time + 1, blizzards
                 )
-            )
-
-
-        # Move down
-        newx, newy = current_loc.pos[0], current_loc.pos[1] + 1
-        if newy <= finish[1] and (newx, newy) not in walls:
-            next_loc.append(
-                State(
-                    (newx, newy),
-                    moves + 1,
-                    blizzards,
-                )
-            )
-
-
-        # Move left
-        newx, newy = current_loc.pos[0] - 1, current_loc.pos[1]
-        if newx > 0 and (newx, newy) not in walls:
-            next_loc.append(
-                State(
-                    (newx, newy),
-                    moves + 1,
-                    blizzards,
-                )
-            )
-
-
-        # Move up
-        newx, newy = current_loc.pos[0], current_loc.pos[1] - 1
-        if newy > 0 and (newx, newy) not in walls:
-            next_loc.append(
-                State(
-                    (newx, newy),
-                    moves + 1,
-                    blizzards,
-                )
-            )
-
-        # Wait
-        next_loc.append(
-            State(
-                current_loc.pos,
-                moves + 1,
-                blizzards,
-            )
-        )
+            ):
+                next_loc.append((newx, newy, time + 1))
 
 
 def part2(lines):
@@ -231,6 +136,6 @@ if __name__ == "__main__":
     # with open(root_path / "sample", "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    print(f"Part 1: Answer: {part1(lines, (1,0), (100, 36))}")
-    # print(f"Part 1: Answer: {part1(lines, (1,0), (6, 5))}")
+    print(f"Part 1: Answer: {part1(lines, (0,0), (100, 35))}")
+    # print(f"Part 1: Answer: {part1(lines, (0,0), (6, 4))}")
     print(f"Part 2: Answer: {part2(lines)}")
