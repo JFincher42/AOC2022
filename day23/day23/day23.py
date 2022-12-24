7a1dd448ccf5d5d8a99b0837326a0f1573457d7d
Jon Fincher <jfincher42@gmail.com>
2022-12-24 11:27:34 -0600

    Day23 done, optimized to use a set instead of a list for the ground - much faster that way

--- day23/day23/day23.py
+++ day23/day23/day23.py
@@ -6,12 +6,12 @@ root_path = pathlib.Path.home() / "git" / "AOC2022" / "day23" / "day23"
 
 
 def parse(lines):
-    ground = []
+    ground = set()
     minx, maxx, miny, maxy = 0, 0, 0, 0
     for y in range(len(lines)):
         for x in range(len(lines[y])):
             if lines[y][x] == "#":
-                ground.append((x, y))
+                ground.add((x, y))
                 minx = min(x, minx)
                 maxx = max(x, maxx)
                 miny = min(y, miny)
@@ -59,7 +59,7 @@ def east(elf, ground):
 
 def part1(lines):
     ground, minx, maxx, miny, maxy = parse(lines)
-    print(f"Before: Elves = {len(ground)}, Min/Max = ({minx}, {miny})/({maxx},{maxy})")
+    # print(f"Before: Elves = {len(ground)}, Min/Max = ({minx}, {miny})/({maxx},{maxy})")
 
     checks = [north, south, west, east]
 
@@ -70,7 +70,7 @@ def part1(lines):
         # And the elf who wants to move there as the value in a list
         # Any list with len > 1 is skipped.
         proposed = {}
-        newground = []
+        newground = set()
 
         # Check each elf each round
         for elf in ground:
@@ -91,18 +91,18 @@ def part1(lines):
 
                 # We couldn't move anywhere, so just add this elf
                 if check:
-                    newground.append(elf)
+                    newground.add(elf)
 
             # Here, we know we're alone, so just add this elf to the new ground
             else:
-                newground.append(elf)
+                newground.add(elf)
 
         # Here, we've processed all the elves this round
         # Let's see who can move
 
         for newelf, oldelf in proposed.items():
             if len(oldelf) == 1:
-                newground.append(newelf)
+                newground.add(newelf)
                 # Update the min and max
                 minx = min(minx, newelf[0])
                 maxx = max(maxx, newelf[0])
@@ -111,7 +111,7 @@ def part1(lines):
             else:
                 # Just append the elves who couldn't move
                 for elf in oldelf:
-                    newground.append(elf)
+                    newground.add(elf)
 
         # We have new ground, so replace it
         ground = newground
@@ -122,7 +122,7 @@ def part1(lines):
 
     # Let's figure out the empty spaces
 
-    print(f"After: Elves = {len(ground)}, Min/Max = ({minx}, {miny})/({maxx},{maxy})")
+    # print(f"After: Elves = {len(ground)}, Min/Max = ({minx}, {miny})/({maxx},{maxy})")
 
     width = maxx - minx + 1
     height = maxy - miny + 1
@@ -130,13 +130,85 @@ def part1(lines):
 
 
 def part2(lines):
-    pass
+    ground, minx, maxx, miny, maxy = parse(lines)
+
+    checks = [north, south, west, east]
+
+    # Go until we stop moving
+    moved = True
+    round = 0
+    while moved:
+        # We need a place to store proposed moves
+        # Store it as a dict, with the location to move as a key
+        # And the elf who wants to move there as the value in a list
+        # Any list with len > 1 is skipped.
+        proposed = {}
+        newground = set()
+
+        # Assume we haven't moved at all
+        moved = False
+
+        # New round
+        round += 1
+
+        # Check each elf each round
+        for elf in ground:
+
+            # Are we alone?
+            if alone(elf, ground):
+
+                # Check each direction
+                for d in range(4):
+                    check, x, y = checks[d](elf, ground)
+                    if not check:
+                        newelf = (elf[0] + x, elf[1] + y)
+                        if newelf not in proposed.keys():
+                            proposed[newelf] = []
+                        proposed[newelf].append(elf)
+                        # No need to check further
+                        break
+
+                # We couldn't move anywhere, so just add this elf
+                if check:
+                    newground.add(elf)
+
+            # Here, we know we're alone, so just add this elf to the new ground
+            else:
+                newground.add(elf)
+
+        # Here, we've processed all the elves this round
+        # Let's see who can move
+
+        for newelf, oldelf in proposed.items():
+            if len(oldelf) == 1:
+                newground.add(newelf)
+                # Update the min and max
+                minx = min(minx, newelf[0])
+                maxx = max(maxx, newelf[0])
+                miny = min(miny, newelf[1])
+                maxy = max(maxy, newelf[1])
+                # We've moved an elf, so indicate that
+                moved = True
+            else:
+                # Just append the elves who couldn't move
+                for elf in oldelf:
+                    newground.add(elf)
+
+        # We have new ground, so replace it
+        ground = newground
+
+        # Now we can rotate the direction
+        checks.append(checks[0])
+        checks = checks[1:]
+
+    # How many rounds did we do?
+    return round
 
 
 if __name__ == "__main__":
 
     with open(root_path / "input", "r") as f:
-    # with open(root_path / "sample", "r") as f:
+        # with open(root_path / "sample", "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
     print(f"Part 1: Answer: {part1(lines)}")
